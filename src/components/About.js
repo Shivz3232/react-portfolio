3c663117c532950fdb9f03b9f34462f7288af1ba
Shivu D G <dgshivu3232@gmail.com>
2021-03-23 11:24:04 +0530

    removed bg image in home and about

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import sanityClient from "../client";
-import profileBackground from "../profileBackground.jpg";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
@@ -33,25 +32,19 @@ function About() {
   if (!author) return <div>Loading...</div>;
 
   return (
-    <main className="relative">
-      <img
-        src={profileBackground}
-        alt="Profile Background"
-        className="absolute w-full"
-      />
+    <main className="relative bg-black min-h-screen p-12">
       <div className="p-10 lg:pt-48 container mx-auto relative">
-        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+        <section className="bg-white rounded-lg shadow-2xl lg:flex p-20">
           <img
-            src={urlFor(author.authorImage).url()}
+            src={urlFor(author.authorImage).width(200).height(200).url()}
             alt={author.name}
             className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
           />
           <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive text-6xl text-green-300 mb-4">
-              Hey there, I'm{" "}
-              <span className="text-green-100">{author.name}</span>
+            <h1 className="cursive text-6xl text-black mb-4">
+              Hey there, I'm <span className="text-red-700">{author.name}</span>
             </h1>
-            <div className="prose lg:prose-xl text-white">
+            <div className="prose lg:prose-xl text-black">
               <BlockContent
                 blocks={author.bio}
                 projectId="ww4grxeo"
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,8 @@
 import React from "react";
-import image from "../rgb.jpg";
 
 function Home() {
   return (
-    <main>
-      <img
-        src={image}
-        alt="Crushed Paper"
-        className="absolute object-cover w-full h-full"
-      />
+    <main className="relative bg-black min-h-screen p-6">
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
         <h1 className="text-6xl text-white font-bold serif leading-none lg:leading-snug home-name">
           Namaste. I'm Shivu.
