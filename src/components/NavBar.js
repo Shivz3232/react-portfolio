26efbca674f7002e08c8449da02119b3e9496fe4
Shivu D G <dgshivu3232@gmail.com>
2021-05-10 11:59:38 +0530

    navbar responsiveness fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,14 +6,14 @@ function NavBar() {
     return (
         <div className="antialiased bg-black">
             <header className="lg:px-16 px-6 bg-bloack flex flex-wrap items-center lg:py-0 py-2">
-                <div classNama="flex-1 flex justify-between items-center">
+                <div className="flex-1 flex justify-between items-center">
                     <NavLink to="/" exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 text-white hover:text-red-900 text-4xl font-bold cursive tracking-widest">
                         D G Shivu
                     </NavLink>
                 </div>
 
                 <label for="menu-toggle" className="cursor-pointer lg:hidden block">
-                    <svg class="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg>
+                    <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg>
                 </label>
                 <input type="checkbox" className="hidden" id="menu-toggle"/>
                 <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
