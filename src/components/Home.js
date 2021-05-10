9886e8c571f6217646810e9ea1247aa957274663
Shivu D G <dgshivu3232@gmail.com>
2021-05-10 12:58:47 +0530

    made home component responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,13 @@
-import React from "react";
+import React from 'react'
 
 function Home() {
-  return (
-    <main className="relative bg-black min-h-screen p-6">
-      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 lg:pb-64 px-8">
-        <h1 className="m-auto text-6xl text-white font-bold serif leading-none lg:leading-snug home-name">
-          Namaste. I'm Shivu.
-        </h1>
-      </section>
-    </main>
-  );
+    return (
+        <div className="flex justify-center w-full min-h-screen bg-black">
+            <h1 className="m-auto font-bold text-center text-white text-5xl lg:text-9xl">
+                Namaste. I'm Shivu.
+            </h1>
+        </div>
+    )
 }
 
-export default Home;
+export default Home
