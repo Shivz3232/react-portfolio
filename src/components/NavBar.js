a65bec2454f6f422ac734d215a8682ec600950fc
Shivu D G <dgshivu3232@gmail.com>
2021-05-11 12:44:49 +0530

    fixed navbar errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,8 +12,8 @@ function NavBar() {
                     </NavLink>
                 </div>
 
-                <label for="menu-toggle" className="cursor-pointer lg:hidden block">
-                    <svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg>
+                <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden block">
+                    <svg className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><title>menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg>
                 </label>
                 <input type="checkbox" className="hidden" id="menu-toggle"/>
                 <div className="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
