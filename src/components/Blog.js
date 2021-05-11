7c85649073e6282019b16b5be34d18d56d6f7925
Shivu D G <dgshivu3232@gmail.com>
2021-05-11 12:46:44 +0530

    removed unwanted imports and variables

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,17 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
 import loadingImage from "../loading.svg";
 
 import sanityClient from "../client";
-
-const builder = imageUrlBuilder(sanityClient);
-
-function urlFor(source) {
-  return builder.image(source);
-}
 
 function Blog() {
   const [blogData, setBlog] = useState(null);
