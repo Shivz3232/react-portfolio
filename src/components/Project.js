e67fb70bda3033bfe52858ddc359f897fb0e010f
Shivu D G <dgshivu3232@gmail.com>
2021-05-11 15:56:15 +0530

    made project component responsive

--- src/components/Project.js
+++ src/components/Project.js
@@ -35,21 +35,21 @@ function Project() {
   return (
     <main className="bg-black min-h-screen p-12">
       <section className="container mx-auto">
-        <h1 className="text-white text-5xl flex justify-center cursive">
+        <h1 className="text-white text-5xl flex text-center justify-center cursive">
           My Projects
         </h1>
         <br />
-        <h2 className="text-white text-lg flex justify-center cursive mb-12">
+        <h2 className="text-white text-lg text-center flex justify-center cursive mb-12">
           Welcome to my projects page.
         </h2>
-        <section className="grid grid-cols-2 gap-8">
+        <section className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           {projectData &&
             projectData.map((project, index) => (
               <article
-                className="relative rounded-lg shadow-xl bg-white p-16"
+                className="relative rounded-lg shadow-xl w-full bg-white p-8 lg:p-16"
                 key={index}
               >
-                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                <h3 className="text-gray-800 text-center text-3xl font-bold mb-2 hover:text-red-700">
                   <a
                     href={project.link}
                     alt={project.title}
@@ -59,34 +59,37 @@ function Project() {
                     {project.title}
                   </a>
                 </h3>
-                <div className="text-gray-500 text-xs space-x-4">
-                  <span>
-                    <strong className="font-bold">Finished on</strong>:{" "}
-                    {new Date(project.date).toLocaleDateString()}
-                  </span>
-                  <span>
-                    <strong className="font-bold">Company</strong>:{" "}
-                    {project.place}
-                  </span>
-                  <span>
-                    <strong className="font-bold">Type</strong>:{" "}
-                    {project.projectType}
-                  </span>
-                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
-                    {project.description}
-                  </p>
-                  <a
-                    href={project.link}
-                    alt={project.title}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-red-500 text-xl font-bold hover:text-red-400"
-                  >
-                    View The Project
-                    <span role="img" aria-label="right pointer">
-                      👉
+                <div className="text-gray-500 text-xs">
+                  <div className="flex justify-center">
+                    <span className="m-1 p-1">
+                      <strong className="font-bold">Finished on</strong>:{" "}
+                      {new Date(project.date).toLocaleDateString()}
                     </span>
-                  </a>
+                    <span className="m-1 p-1">
+                      <strong className="font-bold">Company</strong>:{" "}
+                      {project.place}
+                    </span>
+                    <span className="m-1 p-1">
+                      <strong className="font-bold">Type</strong>:{" "}
+                      {project.projectType}
+                    </span>
+                  </div>
+                  <div>
+                    <p className="my-6 text-lg text-center text-gray-700 leading-relaxed">
+                      {project.description}
+                    </p>
+                  </div>
+                  <div className="flex justify-center">
+                    <a
+                      href={project.link}
+                      alt={project.title}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-red-500 text-xl font-bold underline hover:text-red-400"
+                    >
+                      View The Project
+                    </a>
+                  </div>
                 </div>
               </article>
             ))}
